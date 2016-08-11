f723ec50d74503e5b91b5d5eba150bdd61c50eb0
Anthony <anthony@Anthony-Laptop.Home>
2016-08-11 15:22:27 +0100

    u

--- streamer/static/streamer/js/uploads.js
+++ streamer/static/streamer/js/uploads.js
@@ -1,11 +1,7 @@
-var value;
+
 function show_del_button(element) {
   var es = document.getElementById('del_button');
+  es.value = element;
   es.style.display = 'table-cell';
   value = element;
-}
-
-function get_value(){
-  console.log(value);
-  return value;
 }
